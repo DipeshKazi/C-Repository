6eaf40f6cdd0d8931c3e6c20558012060fba4e7b
DipeshKazi <dipesh2025@hotmail.com>
2017-04-18 11:29:16 -0500

    Update

---
+++ 4.5randomGame/4.5randomGame/Source.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+
+int main()
+{
+	int Uinput; 
+
+	int hard = 1, int medium = 2, int essy = 3;
+
+	std::cout << "Press 1 for Hard , Press 2 for Medium , Press 3 for essy" << std::endl;
+	std::cin >> Uinput;
+
+}
