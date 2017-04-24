9f128cbdfb38ad0e21d8e030dc038648038144be
DipeshKazi <dipesh2025@hotmail.com>
2017-04-24 10:42:22 -0500

    Update

--- 4.5randomGame/4.5randomGame/Source.cpp
+++ 4.5randomGame/4.5randomGame/Source.cpp
@@ -1,98 +1,108 @@
-#include<iostream>
-#include<string>
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+///char chr;
+
 
 int main()
 {
-
-	int randomNumber = rand();
-	int randomNumber2 = rand();
-	int randomNumber3 = rand();
+	int number;
+	int guess;
+	int tries;
 
 	int Uinput;
-	int number;
-	int number2;
-	int number3;
-	int guess;
-	int guess2;
-	int guess3;
+	
 
+	std::string answer;
 
-	int chances = 0;
-	int chances2 = 0;
-	int chances3 = 0;
 	int hard = 1,  medium = 2,  essy = 3;
 
 	std::cout << "Press 1 for Hard , Press 2 for Medium , Press 3 for easy" << std::endl;
 	std::cin >> Uinput;
 
-	if (Uinput == 1) {
+	if (Uinput = 1)
+	{
 
-		do {
-			chances++;
-			(randomNumber % 1000) + 1;
-			std::cout << "Pick a number between 1 and 1000" << std::endl;
-			std::cin >> number;
+		do
+		{
+			number = rand() % 1000 + 1;;
+			std::cout << "Enter a number between 1 and 100" << std::endl;
+			std::cin >> guess;
+			if (number<guess)
+				std::cout << "Too high try again" << std::endl;
+			tries = 1;
+		} while (number>guess);
+		std::cout << "Too low try again" << std::endl;
+		tries--;
+		if (number == guess)
+			std::cout << " You won  " << std::endl;
+		std::cout << "You got the right number in " << tries << " tries";
+		do
+		{
+			std::cout << "Would you like to play again?  Enter Yes";
+			std::cin >> answer;
+			
+		} while (answer == " Yes");
+	}
 
-			if (number > guess)
-			{
-				std::cout << "You chosse too high" << std::endl;
-			}
 
-			else if (number == guess || chances != 2)
-			{
-				std::cout << "You won the game" << std::endl;
-				std::cout << "You have used" << chances << "of 2 chnages" << std::endl;
-			}
+	if (Uinput = 2)
+	{
 
-		} while (chances != 2 || number == guess );
-		std::cout << "Game Over" << chances << "of 5 chances" << std::endl;
+		do
+		{
+			number = rand() % 500 + 1;;
+			std::cout << "Enter a number between 1 and 100" << std::endl;
+			std::cin >> guess;
+			if (number<guess)
+				std::cout << "Too high try again" << std::endl;
+			tries = 3;
+		} while (number>guess);
+		std::cout << "Too low try again" << std::endl;
+		tries--;
+		if (number == guess)
+			std::cout << "You Won  " << std::endl;
+		std::cout << "You got the right number in " << tries << " tries";
+		do
+		{
+			std::cout << "Would you like to play again?  Enter Yes";
+			std::cin >> answer;
+
+		} while (answer == " Yes");
+	}
+
+
+	if (Uinput = 3)
+	{
 	
+		do
+		{
+			number = rand() % 100 + 1;;
+			std::cout << "Enter a number between 1 and 100" << std::endl;
+			std::cin >> guess;
+			if (number<guess)
+				std::cout << "Too high try again" << std::endl;
+			tries = 5;
+		} while (number>guess);
+		std::cout << "Too low try again" << std::endl;
+		tries--;
+		if (number == guess)
+			std::cout << " You Won  " << std::endl;
+		std::cout << "You got the right number in " << tries << " tries";
+		do
+		{
+			std::cout << "Would you like to play again?  Enter Yes";
+			std::cin >> answer;
+
+		} while (answer == " Yes");
 	}
 
-	if (Uinput == 2){
-		do {
-			chances2++;
-			(randomNumber2 % 100) + 1;
-			std::cout << "Pick a number between 1 and 1000" << std::endl;
-			std::cin >> number2;
 
-			if (number2 < guess2)
-			{
-				std::cout << "You chosse too high" << std::endl;
-			}
+	system("pause");
+	return 0;
 
-			else if (number2 == guess2 || chances2 != 4)
-			{
-				std::cout << "You won the game" << std::endl;
-				std::cout << "You have used" << chances2 << "of 2 chnages" << std::endl;
-			}
-
-		} while (chances2 != 4 || number2 == guess2);
-		std::cout << "Game Over" << chances2 << "of 5 chances" << std::endl;
-	
-	}
-
-	if (Uinput == 3) {
-		do {
-			chances++;
-			(randomNumber3 % 50) + 1;
-			std::cout << "Pick a number between 1 and 1000" << std::endl;
-			std::cin >> number3;
-
-			if (number3 < guess3)
-			{
-				std::cout << "You chosse too high" << std::endl;
-			}
-
-			else if (number3 == guess3 || chances3 != 6)
-			{
-				std::cout << "You won the game" << std::endl;
-				std::cout << "You have used" << chances3 << "of 2 chnages" << std::endl;
-			}
-
-		} while (chances3 != 6 || number3 == guess3);
-		std::cout << "Game Over" << chances3 << "of 5 chances" << std::endl;
-		system("pause");
-		return 0;
-	}
 }
+	
+
+	
