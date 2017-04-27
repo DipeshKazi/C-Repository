8647235abc7d93042be731f9eec8a1da3ece8add
DipeshKazi <dipesh2025@hotmail.com>
2017-04-27 10:43:13 -0500

    Udate

--- MadLips/MadLips/Source.cpp
+++ MadLips/MadLips/Source.cpp
@@ -5,7 +5,11 @@ int main()
 {
 	std::cout << "Welcome to Middle Earth " << std::endl;
 	std::string color, animal, BodyPart, noun, Pluralnoun, wordendinginest; 
-	 
+	int number;
+
+
+	std::cout << "Enter a number between 1 to 20" << std::endl;
+	std::cin >> number; 
 
 	std::cout << "Enter the color for dargon. " << std::endl;
 	std::cin >> color; 
@@ -25,12 +29,20 @@ int main()
 	std::cout << "Enter a plural noun " << std::endl;
 	std::cin >> Pluralnoun; 
 
+
+
+	if (number < (1) || number >(20)) 
+	{
+		std::cout << " The number need to be between 1 and 20 " << std::endl;
+		std::cin >> number;
+	}
+
+
+	std::cout << number << " Dargon live in the Gold Mountain. " << std::endl;
 	std::cout << " The " << color << "Dargon is the " << wordendinginest << " dargon of all." << std::endl;
 	std::cout << "It has " << BodyPart << "and a " << animal << "shaped like a " << noun << std::endl; 
 	std::cout << "it loves to eat" << Pluralnoun << ", although it will feast on nearly anything" << std::endl;
 
-
-
-	
-
+	system("pause");
 }
+
