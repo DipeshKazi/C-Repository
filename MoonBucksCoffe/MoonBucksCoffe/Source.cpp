d30b666e9b6168b5e5e8d9d52d7dc8eb4c3fe558
DipeshKazi <dipesh2025@hotmail.com>
2017-05-03 10:33:33 -0500

    Update

---
+++ MoonBucksCoffe/MoonBucksCoffe/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+ 
+double getTotal(double poundsUsed[], int numELements);
+
+int main(){
+	double pounds[12] = { 400.5 , 450.0, 475.5 , 336.5, 457.0, 325.0 , 220.5, 276.0, 300.0, 320.5, 400.5, 415.0};
+
+	double total = 0.0;
+	double average = 0.0; 
+
+	total = getTotal(pounds, 12); 
+	average = total / 12;
+
+	std::cout << "Total pounds:" << total << std::endl;
+	std::cout << "Avarage pounds" << average << std::endl;
+
+	system("pause"); 
+	return 0;
+}
+
+double getTotal(double poundsUsed[], int numELements)
+{
+	double total
+}
