b034038fd7323f5acdefe6a0c54499fab0d4b2f5
DipeshKazi <dipesh2025@hotmail.com>
2017-05-04 11:13:55 -0500

    Update

--- MoonBucksCoffe/MoonBucksCoffe/Source.cpp
+++ MoonBucksCoffe/MoonBucksCoffe/Source.cpp
@@ -3,11 +3,16 @@
 double getTotal(double poundsUsed[], int numELements);
 
 int main(){
+
+	// Declare array
 	double pounds[12] = { 400.5 , 450.0, 475.5 , 336.5, 457.0, 325.0 , 220.5, 276.0, 300.0, 320.5, 400.5, 415.0};
 
+
+	//Declare variables 
 	double total = 0.0;
 	double average = 0.0; 
 
+	//Calculate Total & Average 
 	total = getTotal(pounds, 12); 
 	average = total / 12;
 
@@ -20,5 +25,8 @@ int main(){
 
 double getTotal(double poundsUsed[], int numELements)
 {
-	double total
+	double totalUsed = 0.0; //accumulator 
+
+	for (int sub = 0; sub < numELements; sub += 1)
+		totalUsed += poundsUsed[sub];
 }
